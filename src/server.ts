c4f0b1315d2546edd11cfda2a000355e6fc91921
PangNem <sprnd645@gmail.com>
2020-04-25 23:57:07 +0900

    Update sequelize sync then app listen

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,19 @@
 import app from './app';
 import { errorHandler } from './middlewares/error-handler';
+import { sequelize } from './models';
 
 const host: string = app.get('host');
 const port: number = app.get('port');
 
 app.use(errorHandler);
+
+sequelize.sync({ force: false })
+  .then(() => {
+    console.log('DB Connect Success!');
+  })
+  .catch((err: Error) => {
+    console.error(err);
+  });
 
 app.listen(port, () => {
   console.log(`✅ Server is: http://${host}:${port}`);
