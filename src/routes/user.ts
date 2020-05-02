c57bc8ca7a766592d10d552c81b99775208cd03c
PangNem <sprnd645@gmail.com>
2020-05-02 21:03:23 +0900

    Delete controllers

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,8 +1,22 @@
 import { Router } from 'express';
-import * as user from '../controllers/user';
+import UserService from '../services/UserService';
+import Container from 'typedi';
+import logger from '../utils/logger';
 
 const router = Router();
 
-router.post('/', user.create);
+router.post('/', async (req, res, next) => {
+  try {
+    const userDTO = req.body;
+
+    const userServiceInstance = Container.get(UserService);
+    const { id, nickname, createdAt, updatedAt } = await userServiceInstance.createUser(userDTO);
+
+    return res.status(201).json({ id, nickname, createdAt, updatedAt });
+  } catch (error) {
+    logger.error(error);
+    return next(error);
+  }
+});
 
 export default router;
