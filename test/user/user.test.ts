e2d47d3f306013b8dcc24489156d956690d2c63e
PangNem <sprnd645@gmail.com>
2020-05-15 01:04:18 +0900

    Change user test code describe

--- test/user/user.test.ts
+++ test/user/user.test.ts
@@ -3,13 +3,12 @@ import app from '../../src/app';
 import sequelize from '../../src/models/sequelize';
 import User from '../../src/models/User';
 
-describe('POST /user', () => {
-  const url = '/user';
-  const testData = { 'nickname': 'pangnem' };
-
+describe('user test', () => {
   let testUserId: number;
 
-  beforeAll(async done => {
+  const testData = { nickname: 'pangnem' };
+
+  beforeAll(async (done) => {
     await sequelize.sync();
     const testUser = await User.create({ testData });
 
@@ -17,28 +16,28 @@ describe('POST /user', () => {
     done();
   });
 
-  afterAll(async done => {
+  afterAll(async (done) => {
     await User.destroy({ where: { id: testUserId } });
     done();
   });
 
-  describe('유저 생성 성공시', () => {
-    test('유저 닉네임 반환', async () => {
-      const response = await request(app)
-        .post(url)
-        .send(testData);
+  describe('POST /user', () => {
+    const url = '/user';
 
-      expect(response.status).toBe(201);
-      expect(response.body.data.nickname).toBe('pangnem');
+    describe('유저 생성 성공시', () => {
+      test('유저 닉네임 반환', async () => {
+        const response = await request(app).post(url).send(testData);
+
+        expect(response.status).toBe(201);
+        expect(response.body.data.nickname).toBe('pangnem');
+      });
     });
-  });
-  describe('유저 생성 실패시', () => {
-    test('형식에 맞지 않는 데이터 요청시 422 반환', async () => {
-      const response = await request(app)
-        .post(url)
-        .send({ 'qwer': 'asdf' });
+    describe('유저 생성 실패시', () => {
+      test('형식에 맞지 않는 데이터 요청시 422 반환', async () => {
+        const response = await request(app).post(url).send({ qwer: 'asdf' });
 
-      expect(response.status).toBe(422);
+        expect(response.status).toBe(422);
+      });
     });
   });
 });
